06c863917b09308bfe0a3c93e823726b6e782dd1
isabellymazurek-ops <isabelly.mazurek@escola.pr.gov.br>
2026-05-27 12:19:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById("particles");
+const canvas = document.getElementById("bg");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
@@ -6,66 +6,34 @@ canvas.height = window.innerHeight;
 
 let particles = [];
 
-class Particle {
-  constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
+for(let i = 0; i < 120; i++){
+  particles.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    r: Math.random() * 2,
+    dx: (Math.random() - 0.5),
+    dy: (Math.random() - 0.5)
+  });
+}
 
-    // evita raio 0
-    this.radius = Math.random() * 3 + 1;
+function animate(){
+  ctx.clearRect(0,0,canvas.width,canvas.height);
 
-    this.speedY = Math.random() * 1 + 0.5;
-    this.color = `hsl(${Math.random() * 360}, 100%, 70%)`;
-  }
+  ctx.fillStyle = "#00ffe5";
 
-  update() {
-    this.y -= this.speedY;
-
-    // reaparece embaixo
-    if (this.y < -this.radius) {
-      this.y = canvas.height + this.radius;
-      this.x = Math.random() * canvas.width;
-    }
-  }
-
-  draw() {
+  particles.forEach(p => {
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-
-    ctx.fillStyle = this.color;
-
-    ctx.shadowBlur = 20;
-    ctx.shadowColor = this.color;
-
+    ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2);
     ctx.fill();
-  }
-}
 
-function init() {
-  particles = [];
+    p.x += p.dx;
+    p.y += p.dy;
 
-  for (let i = 0; i < 180; i++) {
-    particles.push(new Particle());
-  }
-}
-
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  particles.forEach((p) => {
-    p.update();
-    p.draw();
+    if(p.x < 0 || p.x > canvas.width) p.dx *= -1;
+    if(p.y < 0 || p.y > canvas.height) p.dy *= -1;
   });
 
   requestAnimationFrame(animate);
 }
 
-init();
 animate();
-
-window.addEventListener("resize", () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  init();
-});
